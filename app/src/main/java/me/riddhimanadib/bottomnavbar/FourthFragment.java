a05d88e1f6caf78989fc8b59970c472e3fe2b139
purumLee <purum7765@gmail.com>
2018-11-11 22:26:41 +0900

    프로필 탭

--- app/src/main/java/me/riddhimanadib/bottomnavbar/FourthFragment.java
+++ app/src/main/java/me/riddhimanadib/bottomnavbar/FourthFragment.java
@@ -3,22 +3,210 @@ package me.riddhimanadib.bottomnavbar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.mingle.entity.MenuEntity;
+import com.mingle.sweetpick.BlurEffect;
+import com.mingle.sweetpick.CustomDelegate;
+import com.mingle.sweetpick.DimEffect;
+import com.mingle.sweetpick.RecyclerViewDelegate;
+import com.mingle.sweetpick.SweetSheet;
+import com.mingle.sweetpick.ViewPagerDelegate;
+
+import java.util.ArrayList;
 
 /**
  * Created by Adib on 13-Apr-17.
  */
 
 public class FourthFragment extends Fragment{
+    private SweetSheet mSweetSheet;
+    private SweetSheet mSweetSheet2;
+    private SweetSheet mSweetSheet3;
+    private RelativeLayout rl;
 
     public static FourthFragment newInstance() {
         return new FourthFragment();
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fourth, container, false);
+         View view =inflater.inflate(R.layout.activity_main, container, false);
+        rl = (RelativeLayout) view.findViewById(R.id.rl);
+         setupViewpager();
+        setupRecyclerView();
+        setupCustomView();
+        setHasOptionsMenu(true);
+        return view;
+
+        //rl = (RelativeLayout)  findViewById(R.id.rl);
+
+    }
+
+
+    private void setupCustomView() {
+
+
+        mSweetSheet3 = new SweetSheet(rl);
+        CustomDelegate customDelegate = new CustomDelegate(true,
+                CustomDelegate.AnimationType.DuangLayoutAnimation);
+        View view = LayoutInflater.from(FourthFragment.super.getActivity()).inflate(R.layout.layout_custom_view, null, false);
+        customDelegate.setCustomView(view);
+        mSweetSheet3.setDelegate(customDelegate);
+        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSweetSheet3.dismiss();
+            }
+        });
+    }
+
+    private void setupRecyclerView() {
+
+        final ArrayList<MenuEntity> list = new ArrayList<>();
+        //添加假数据
+        MenuEntity menuEntity1 = new MenuEntity();
+        menuEntity1.iconId = R.drawable.ic_account_child;
+        menuEntity1.titleColor = 0xff000000;
+        menuEntity1.title = "code";
+        MenuEntity menuEntity = new MenuEntity();
+        menuEntity.iconId = R.drawable.ic_account_child;
+        menuEntity.titleColor = 0xffb3b3b3;
+        menuEntity.title = "식사";
+        list.add(menuEntity1);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        // SweetSheet 控件,根据 rl 确认位置
+        mSweetSheet = new SweetSheet(rl);
+
+        //设置数据源 (数据源支持设置 list 数组,也支持从菜单中获取)
+        mSweetSheet.setMenuList(list);
+        //根据设置不同的 Delegate 来显示不同的风格.
+        mSweetSheet.setDelegate(new RecyclerViewDelegate(true));
+        //根据设置不同Effect 来显示背景效果BlurEffect:模糊效果.DimEffect 变暗效果
+        mSweetSheet.setBackgroundEffect(new BlurEffect(8));
+        //设置点击事件
+        mSweetSheet.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+            @Override
+            public boolean onItemClick(int position, MenuEntity menuEntity1) {
+                //即时改变当前项的颜色
+                list.get(position).titleColor = 0xff5823ff;
+                ((RecyclerViewDelegate) mSweetSheet.getDelegate()).notifyDataSetChanged();
+
+                //根据返回值, true 会关闭 SweetSheet ,false 则不会.
+                //Toast.makeText(FourthActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+
+    }
+
+    private void setupViewpager() {
+
+
+        mSweetSheet2 = new SweetSheet(rl);
+
+        //从menu 中设置数据源
+        mSweetSheet2.setMenuList(R.menu.menu_sweet);
+        mSweetSheet2.setDelegate(new ViewPagerDelegate());
+        mSweetSheet2.setBackgroundEffect(new DimEffect(0.5f));
+        mSweetSheet2.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+            @Override
+            public boolean onItemClick(int position, MenuEntity menuEntity1) {
+
+            //    Toast.makeText(FourthActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+
+    /*
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+           eturn true;
+        }
+    /*
+        @Override
+        public void onBackPressed() {
+
+            if (mSweetSheet.isShow() || mSweetSheet2.isShow()) {
+                if (mSweetSheet.isShow()) {
+                    mSweetSheet.dismiss();
+                }
+                if (mSweetSheet2.isShow()) {
+                    mSweetSheet2.dismiss();
+                }
+            } else {
+                super.onBackPressed();
+            }
+
+        }
+    */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_recyclerView) {
+            if (mSweetSheet2.isShow()) {
+                mSweetSheet2.dismiss();
+            }
+            if (mSweetSheet3.isShow()) {
+                mSweetSheet3.dismiss();
+            }
+            mSweetSheet.toggle();
+
+            return true;
+        }
+        if (id == R.id.action_viewpager) {
+            if (mSweetSheet.isShow()) {
+                mSweetSheet.dismiss();
+            }
+            if (mSweetSheet3.isShow()) {
+                mSweetSheet3.dismiss();
+            }
+            mSweetSheet2.toggle();
+            return true;
+        }
+        if (id == R.id.action_custom) {
+            if (mSweetSheet.isShow()) {
+                mSweetSheet.dismiss();
+            }
+            if (mSweetSheet2.isShow()) {
+                mSweetSheet2.dismiss();
+            }
+            mSweetSheet3.toggle();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
